454b195d66cb1eb233187afffa18aece012ca85a
drib <calumholt@hotmail.com>
2023-11-01 14:19:20 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<ul>
+  <li>
+    <a href="/">Home</a>
+  <li>
+      <a href="/gallery">Gallery</a>
+  </li>
+  <li>
+    <a href="/store">Store</a>
+  </li>
+  <li>
+    <a href="/contact">Contact</a>
+  </li>
+</ul>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
