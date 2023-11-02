f70c46bb3de3fab373892b1b5ab40a631bedddd0
drib <calumholt@hotmail.com>
2023-11-02 11:16:19 -0600

    added component folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,20 @@
   // Getting rid of document.querySelector
   let test = "testing";
 
+  // rendering html content
+  let hello = "<span>is this working?</span>";
 </script>
 <ul>
-  <li>
-    <a href="/">Home</a>
-  <li>
+  <li class="text-sky-500">
       <a href="/gallery">Gallery</a>
   </li>
-  <li>
+  <li class="text-red-500">
     <a href="/store">Store</a>
   </li>
-  <li>
+  <li class="text-yellow-500">
     <a href="/contact">Contact</a>
   </li>
 </ul>
 <!-- attach the variable directly to the html -->
-<h1>SvelteKit Sandbox {test}</h1>
+<h1>SvelteKit Sandbox {@html hello}</h1>
 
